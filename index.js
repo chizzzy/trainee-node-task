3e5a9cea88ae3ea5904a19930389bc7765401e84
chizzz <mgandrey99@gmail.com>
2019-02-22 16:30:20 +0200

    implement handler for request that gets courses of given student by id

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const express = require("express");
 const app = express();
 const fs = require('fs');
 const students = fs.readFileSync('students.json', 'utf-8');
-const courses = fs.readFileSync('courses.json', 'utf-8');
+const courses = JSON.parse(fs.readFileSync('courses.json', 'utf-8'));
 const evaluation = fs.readFileSync('evaluation.json', 'utf-8');
 
-
-app.get("/url", (req, res, next) => {
-    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+app.get('/api/students', (req, res) => {
+    res.send(students);
 });
+app.get('/api/courses/:id', (req, res) => {
+        const studentCourses = courses.filter(course => course.studentId === parseInt(req.params.id));
+        if (!studentCourses) res.status(404).send('The course with given ID was not found');
+        res.send(studentCourses);
+    });
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
 
